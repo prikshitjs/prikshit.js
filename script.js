1f353660bbf7bb3316a84cd27f9c9028931e3fc0
Prikshit js <prikshitxcode@gmail.com>
2024-11-19 16:05:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const bgcolor = ["#f18e00", "#00beeb", "#9100eb", "#a7d600"];
+let body = document.querySelector("body");
+let menubar = document.querySelector("#menubar");
+let menu    = document.querySelector(".menu");
+
+
+let bgcolorchange = () => {
+  let random1 = bgcolor[Math.floor(Math.random() * bgcolor.length)];
+  console.log(random1);
+  body.style.background = random1;
+  menubar.style.color = random1;
+  menu.style.background = random1+"a1";
+};
+
+setInterval(bgcolorchange,5000);
+
+menubar.addEventListener("click", ()=> {
+    if(menubar.className == "fa-solid fa-bars") {
+        menubar.className = "fa-solid fa-xmark";
+        menu.style.transform = "translateX(0)"
+    }else{
+        menubar.className = "fa-solid fa-bars";
+        menu.style.transform = "translateX(150px)"
+    }
+})
